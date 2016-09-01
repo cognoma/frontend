75bf538629486239d294a6b14dd060c2c3404bcd
Chris Fuller <Chris@keenleo.com>
2016-08-31 20:54:27 -0400

    merge html and single page version
    Merge html and single page version of site into a single repository.

--- app/results/results.js
+++ app/results/results.js
@@ -9,7 +9,12 @@ angular.module('cognomaApp.results', ['ngRoute', 'ui.bootstrap', 'ngAnimate', 'n
   });
 }]);
 
-angular.module('cognomaApp.results').controller('tabsResultsCtrl', function ($scope, $window) {
+angular.module('cognomaApp.results').controller('tabsResultsCtrl', function ($scope, $window, $http) {
+
+  $http.get('model1234.json')
+       .then(function(res){
+          $scope.results = res.data;
+        });
 
   $scope.tabs = [
     { title:'Genes', content:'Dynamic Genes content' },
@@ -24,7 +29,111 @@ angular.module('cognomaApp.results').controller('tabsResultsCtrl', function ($sc
   };
 
   $scope.resultDetails = {
-    name: 'model1234'
+    name: 'model1234',
+    status: 	'Completed - No Errors',
+    algorithm: 	'Two Class Decision Forest',
+    accuracy: 	'75%',
+    fValue: 	'0.73'
   };
 
+});
+
+angular.module('cognomaApp.results').controller('ModalDemoCtrl', function ($uibModal, $log) {
+  var $ctrl = this;
+  $ctrl.items = ['item1', 'item2', 'item3'];
+
+  $ctrl.animationsEnabled = true;
+
+  $ctrl.open = function (size) {
+    var modalInstance = $uibModal.open({
+      animation: $ctrl.animationsEnabled,
+      ariaLabelledBy: 'modal-title',
+      ariaDescribedBy: 'modal-body',
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      controllerAs: '$ctrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $ctrl.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $ctrl.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $ctrl.openComponentModal = function () {
+    var modalInstance = $uibModal.open({
+      animation: $ctrl.animationsEnabled,
+      component: 'modalComponent',
+      resolve: {
+        items: function () {
+          return $ctrl.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $ctrl.selected = selectedItem;
+    }, function () {
+      $log.info('modal-component dismissed at: ' + new Date());
+    });
+  };
+
+  $ctrl.toggleAnimation = function () {
+    $ctrl.animationsEnabled = !$ctrl.animationsEnabled;
+  };
+});
+
+// Please note that $uibModalInstance represents a modal window (instance) dependency.
+// It is not the same as the $uibModal service used above.
+
+angular.module('cognomaApp.results').controller('ModalInstanceCtrl', function ($uibModalInstance, items) {
+  var $ctrl = this;
+  $ctrl.items = items;
+  $ctrl.selected = {
+    item: $ctrl.items[0]
+  };
+
+  $ctrl.ok = function () {
+    $uibModalInstance.close($ctrl.selected.item);
+  };
+
+  $ctrl.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+});
+
+// Please note that the close and dismiss bindings are from $uibModalInstance.
+
+angular.module('cognomaApp.results').component('modalComponent', {
+  templateUrl: 'myModalContent.html',
+  bindings: {
+    resolve: '<',
+    close: '&',
+    dismiss: '&'
+  },
+  controller: function () {
+    var $ctrl = this;
+
+    $ctrl.$onInit = function () {
+      $ctrl.items = $ctrl.resolve.items;
+      $ctrl.selected = {
+        item: $ctrl.items[0]
+      };
+    };
+
+    $ctrl.ok = function () {
+      $ctrl.close({$value: $ctrl.selected.item});
+    };
+
+    $ctrl.cancel = function () {
+      $ctrl.dismiss({$value: 'cancel'});
+    };
+  }
 });
