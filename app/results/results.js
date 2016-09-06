1722327ec51eae83c1790537892f104bddb203f9
Chris Fuller <Chris@keenleo.com>
2016-09-05 23:07:40 -0400

    Merge and Organize static html and angular versions
    Merged files from BobMiller and Keenleo. Static HTML version is in
    html_version folder. Single page app, angular 1.0 version is in the app
    folder

--- app/results/results.js
+++ app/results/results.js
@@ -9,22 +9,134 @@ angular.module('cognomaApp.results', ['ngRoute', 'ui.bootstrap', 'ngAnimate', 'n
   });
 }]);
 
-angular.module('cognomaApp.results').controller('tabsResultsCtrl', function ($scope, $window) {
+angular.module('cognomaApp.results').controller('tabsResultsCtrl', function ($scope, $window, $http, $routeParams) {
+
+  var classification = $routeParams.classification+'.json';
+
+  $http.get(classification)
+       .then(function(res){
+          $scope.resultData = res.data;
+          console.log($scope.resultData.widget.id);
+
+          $scope.resultDetails = {
+            name: $scope.resultData.widget.id,
+            status: 	$scope.resultData.widget.results.Status,
+            algorithm: 	$scope.resultData.widget.results.Algorithm,
+            accuracy: 	$scope.resultData.widget.results.Accuracy,
+            fValue: 	$scope.resultData.widget.results.fValue
+          };
+
+        });
 
   $scope.tabs = [
     { title:'Genes', content:'Dynamic Genes content' },
     { title:'Samples', content:'Dynamic Samples content' },
     { title:'Algorithm', content:'Dynamic Algorithm content' },
-    { title:'Submission', content:'Dynamic Submission content' },
-    { title:'Results', content:'Dynamic Results content' }
   ];
 
   $scope.model = {
     name: 'Tabs'
   };
 
-  $scope.resultDetails = {
-    name: 'model1234'
+
+});
+
+angular.module('cognomaApp.results').controller('ModalDemoCtrl', function ($uibModal, $log) {
+  var $ctrl = this;
+  $ctrl.items = ['item1', 'item2', 'item3'];
+
+  $ctrl.animationsEnabled = true;
+
+  $ctrl.open = function (size) {
+    var modalInstance = $uibModal.open({
+      animation: $ctrl.animationsEnabled,
+      ariaLabelledBy: 'modal-title',
+      ariaDescribedBy: 'modal-body',
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      controllerAs: '$ctrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $ctrl.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $ctrl.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
   };
 
+  $ctrl.openComponentModal = function () {
+    var modalInstance = $uibModal.open({
+      animation: $ctrl.animationsEnabled,
+      component: 'modalComponent',
+      resolve: {
+        items: function () {
+          return $ctrl.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $ctrl.selected = selectedItem;
+    }, function () {
+      $log.info('modal-component dismissed at: ' + new Date());
+    });
+  };
+
+  $ctrl.toggleAnimation = function () {
+    $ctrl.animationsEnabled = !$ctrl.animationsEnabled;
+  };
+});
+
+// Please note that $uibModalInstance represents a modal window (instance) dependency.
+// It is not the same as the $uibModal service used above.
+
+angular.module('cognomaApp.results').controller('ModalInstanceCtrl', function ($uibModalInstance, items) {
+  var $ctrl = this;
+  $ctrl.items = items;
+  $ctrl.selected = {
+    item: $ctrl.items[0]
+  };
+
+  $ctrl.ok = function () {
+    $uibModalInstance.close($ctrl.selected.item);
+  };
+
+  $ctrl.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+});
+
+// Please note that the close and dismiss bindings are from $uibModalInstance.
+
+angular.module('cognomaApp.results').component('modalComponent', {
+  templateUrl: 'myModalContent.html',
+  bindings: {
+    resolve: '<',
+    close: '&',
+    dismiss: '&'
+  },
+  controller: function () {
+    var $ctrl = this;
+
+    $ctrl.$onInit = function () {
+      $ctrl.items = $ctrl.resolve.items;
+      $ctrl.selected = {
+        item: $ctrl.items[0]
+      };
+    };
+
+    $ctrl.ok = function () {
+      $ctrl.close({$value: $ctrl.selected.item});
+    };
+
+    $ctrl.cancel = function () {
+      $ctrl.dismiss({$value: 'cancel'});
+    };
+  }
 });
