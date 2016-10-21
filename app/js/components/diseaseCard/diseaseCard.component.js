835c3e9213c0e9bc70f249c7ecfbd13e3cd7c592
Tanner Wells <tannerjameswells@gmail.com>
2016-10-20 21:53:27 -0400

    adds disease type set and mutations set
    adds components:
    querySetDiseaseType
    querySetMutations
    geneCard
    diseaseCard

---
+++ app/js/components/diseaseCard/diseaseCard.component.js
@@ -0,0 +1,22 @@
+const DiseaseCardComponent = {
+    templateUrl: 'diseaseCard/diseaseCard.tpl.html',
+    bindings: {
+      id: '@',
+      positives: '@',
+      negatives: '@'
+    },
+    transclude: true,
+    controller: function() {
+      this.add = function(n1, n2){
+        n1 = Number(n1);
+        n2 = Number(n2);
+        return n1 + n2;
+      }
+    	'ngInject';
+    }
+}
+
+export default {
+	name: 'diseaseCard',
+	obj: DiseaseCardComponent
+};
