7040dc5ada55684452f7b1266990c0a5ceb7863f
Ben Dolly <ben@1trickpony.com>
2017-01-10 23:20:27 -0500

    DiseaseSearchService:addition of TODO to clean up logic

--- app/js/services/DiseaseSearchService.js
+++ app/js/services/DiseaseSearchService.js
@@ -2,7 +2,15 @@ function DiseaseSearchService($q, DiseaseResource) {
   'ngInject';
 
   const service = {};
-  
+  // TODO:
+  // - Migrate: DiseaseResourceFactory logic to pull in all from  /diseases/
+  // - Transform: each result into a Disease Model 
+  // - GET: disease samples total:  /samples?limit=1&disease=<disease acronym>
+  // - - response: count 
+  // - Build: mutationList_params: query string parameter "mutations__gene=<entrezid>" from mutationList
+  // - GET Positives: for each disease result /samples?limit=1&disease=<disease acronym>&mutationList_params
+  // - - response: count 
+  // - Calculate" disease_negativces: samples_total - disease_positives
   
   service.get = function(searchQuery, mutationList = null) {
   
