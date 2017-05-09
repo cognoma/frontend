a30f0cc1004656314881788163b7ace117943c7f
Ben Dolly <ben@1trickpony.com>
2017-05-08 22:06:40 -0400

    fixes gulp/tasks/development.js conflict

--- gulp/tasks/development.js
+++ gulp/tasks/development.js
@@ -5,10 +5,8 @@ gulp.task('dev', ['clean'], function(cb) {
 
   global.isProd = false;
 
-<<<<<<< HEAD
-  runSequence(['styles', 'images', 'fonts','views'], 'browserify', 'watch', cb);
-=======
+
   runSequence(['styles', 'images', 'fonts','views'], 'browserify','watch', cb);
->>>>>>> db29ca0bad9e3bf3617019a0881965a5475f5ab0
+
 
 });
