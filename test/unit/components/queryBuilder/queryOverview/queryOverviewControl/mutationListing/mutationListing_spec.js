34490233eed73520deaf52d2af8980e43bdf14fa
Ben Dolly <bromeodolly@gmail.com>
2017-06-03 14:45:14 -0400

    updates mutationListing unit test

--- test/unit/components/queryBuilder/queryOverview/queryOverviewControl/mutationListing/mutationListing_spec.js
+++ test/unit/components/queryBuilder/queryOverview/queryOverviewControl/mutationListing/mutationListing_spec.js
@@ -19,7 +19,10 @@ describe('UNIT::component: mutationListing:', () => {
     beforeEach(inject(($compile, $rootScope) => {
       $_rootScope = $rootScope;
       parentScope = $rootScope.$new();
-      // parentScope.listType = 'mutations';
+
+      parentScope.removeParam = jasmine.createSpy('onRemoveParam');
+      parentScope.listType = 'mutations';
+
       parentScope.mutationsList = [
       {
             '_id':        '4331',
@@ -34,10 +37,12 @@ describe('UNIT::component: mutationListing:', () => {
 
         element = angular.element(`
           <div>
-        	   <mutation-listing                
-                ng-repeat="mutation in mutationsList"
-                entrezgene="mutation.entrezgene"
-                symbol="mutation.symbol"
+              <mutation-listing
+                ng-if="listType == 'mutations' "
+                ng-repeat="gene in mutationsList"
+                symbol="gene.symbol"
+                entrezgene="gene.entrezgene"
+                on-remove-param="removeParam({id, paramRef,paramType})"
               ></mutation-listing>
             </div>
         `);
@@ -94,45 +99,14 @@ describe('UNIT::component: mutationListing:', () => {
     });
 
 
-  xdescribe('event emitters:',()=>{
+  it('should call "removeParam" method on parent component',()=>{
+      const removeParamButton = findIn(element, '.glyphicon-remove-circle');
+      removeParamButton.triggerHandler('click');
+ 
+      expect(parentScope.removeParam).toHaveBeenCalledWith({id: 4331, paramRef:'entrezgene', paramType:'mutations'});
+    });
 
-    it('removeMutation() fires mutationSet:remove:mutation event ', ()=>{
-       parentScope.mutationsList =[
-          {
-            '_id': '4331',
-            '_score': 17.74919,
-            'entrezgene': 4331,
-            'name': 'MNAT1, CDK activating kinase assembly factor',
-            'symbol': 'MNAT1',
-            'taxid': 9606
-          },
-          {
-            '_id': '388324',
-            '_score': 15.771275,
-            'entrezgene': 388324,
-            'name': 'inhibitor of CDK, cyclin A1 interacting protein 1',
-            'symbol': 'INCA1',
-            'taxid': 9606
-          },
-          {
-            '_id': '1030',
-            '_score': 0.9556542,
-            'entrezgene': 1030,
-            'name': 'cyclin dependent kinase inhibitor 2B',
-            'symbol': 'CDKN2B',
-            'taxid': 9606
-          }
-
-        ];
-        parentScope.$digest();
-        const mutationSetRemove_spy = jasmine.createSpy('mutationSetRemove_spy');
-        $_rootScope.$on('mutationSet:remove:mutation', mutationSetRemove_spy);
-        
-        const emitEventButton = findIn(element, '.glyphicon-remove-circle');
-        emitEventButton.triggerHandler('click');
-
-        expect(mutationSetRemove_spy).toHaveBeenCalledWith(jasmine.anything(), {entrezgene: parentScope.mutationsList[0].entrezgene});
-      });
-  })
+  
+  
 
 });
