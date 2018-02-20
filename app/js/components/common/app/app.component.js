5a6a92b4658d2185acb7888d324d81fd5455f629
Stephanie Liu <stephofliu@gmail.com>
2018-02-20 15:43:36 -0500

    address code review comments

--- app/js/components/common/app/app.component.js
+++ app/js/components/common/app/app.component.js
@@ -1,50 +1,48 @@
-const template = require('./app.tpl.html');
+const template = require("./app.tpl.html");
 
 const AppComponent = {
   template,
   bindings: {},
-  controller: ['$rootScope','$log','UserAuth','NotificationService', function($rootScope, $log,UserAuth, NotificationService){
-    'ngInject';
-    $log = $log.getInstance('AppComponent', false);
-    $log.log('');
+  controller: [
+    "$rootScope",
+    "$log",
+    "UserAuth",
+    "NotificationService",
+    function($rootScope, $log, UserAuth, NotificationService) {
+      "ngInject";
+      $log = $log.getInstance("AppComponent", false);
+      $log.log("");
 
-    this.$onInit=()=>{
+      this.$onInit = () => {
+        UserAuth.login().then(authenticatedUser => {
+          this.STATE.user = authenticatedUser;
 
-      UserAuth
-      .login()
-      .then(authenticatedUser=>{
-        this.STATE.user = authenticatedUser;
-
-        NotificationService.notify({
-          type:    'success',
-          message: `Logged in as <strong>${authenticatedUser.name}</strong>`
+          NotificationService.notify({
+            type: "success",
+            message: `Logged in as <strong>${authenticatedUser.name}</strong>`
+          });
         });
-      });
+      };
 
-      
+      const initialState = {
+        query: {
+          title: "",
+          mutations: [],
+          diseases: []
+        },
+        user: {}
+      };
 
-    }
-
-    const initialState = {
-      query: {
-        title:     '',
-        mutations: [],
-        diseases:  []
-      },
-      user: {}
-    };
-
-    this.STATE = angular.copy(initialState);
-
-    $rootScope.$on('QUERY_SUBMITTED', () => {
       this.STATE = angular.copy(initialState);
-    });
 
-
-  }]
-}
+      $rootScope.$on("QUERY_SUBMITTED", () => {
+        this.STATE = angular.copy(initialState);
+      });
+    }
+  ]
+};
 
 export default {
- name: 'app',
- obj: AppComponent
+  name: "app",
+  obj: AppComponent
 };
